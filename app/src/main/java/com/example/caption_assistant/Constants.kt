2f912117b50e64cd8a2a8c7214e2c7266b0cb972
slipnitskaya <lipnitskaya.sofya@gmail.com>
2021-03-10 22:40:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/caption_assistant/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.caption_assistant
+
+object Constants {
+    const val IMAGE_COUNTER_KEY = "ImageId"
+    const val IMAGE_FILENAME = "Image"
+    const val IMAGE_FOLDER_NAME = "Images"
+    const val CSV_NAME = "caption_assistant.csv"
+    const val DATABASE_NAME = "caption_assistant.db"
+    const val MODEL_NAME = "model.pt"
+    const val NUM_CHANNELS = 3
+    const val INPUT_HEIGHT = 256
+    const val INPUT_WIDTH = 256
+    const val PROBABILITY_THRESHOLD = 0.9f
+    const val MOMENTUM = 0.1f
+}
